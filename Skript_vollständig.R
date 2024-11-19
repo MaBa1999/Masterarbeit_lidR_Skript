68c7b373c0a15bd2bff5d040ab3120ab71c4b2fb
Marc B. <marc.baudy99@gmail.com>
2024-11-19 10:29:10 +0100

    Update Skript_vollständig.R
    Hinzufügen der Überlappungsfilterung

--- Skript_vollständig.R
+++ Skript_vollständig.R
@@ -5,7 +5,9 @@
 # Betreuer: Herr Prof. Dr. Magdon, Herr Starker
 # Version 1.0
 
+
 ### Vorbereitungen: ###
+
 #Lade nötige R-Packages
 required_packages <- c("lidR", "sf", "ggplot2", "raster", "viridis", "mapview", "future", "parallel", "parallelly", "RCSF", "lasR")
 
@@ -20,4 +22,46 @@ for (pkg in required_packages) {
   }
 }
 
-#Parallel-Prozessing 
+
+## Parallel-Prozessing 
+cores <- parallelly::availableCores()
+#Hier je nach Leistung des PCs einstellen, hier werden nur 1/4 der Threads der CPU genutzt:
+cores <- as.integer(cores/4)
+future::plan(future::multisession, workers = cores)
+lidR::set_lidr_threads(cores)
+
+
+## Arbeitsverzeichnis
+#Hier das Arbeitsverzeichnis einstellen
+base::setwd("C:/Masterarbeit_R")
+
+
+### Daten laden
+
+#LidR catalog
+ctg <- lidR::readLAScatalog("./Daten/Rohdaten/ALS-Daten_solling/04_classified")
+
+### Daten Überprüfen
+
+base::print(ctg)
+lidR::las_check(ctg)
+lidR::plot(ctg, mapview = TRUE)
+
+### Daten Überlappung filtern
+
+#Einstellung der Chunksgröße und des Buffers
+lidR::opt_chunk_buffer(ctg) <- 200
+lidR::opt_chunk_size(ctg) <- 400
+
+#Filtern der Überlappungen
+lidR::opt_laz_compression(ctg) <- TRUE
+lidR::opt_output_files(ctg) <- "./Daten/Zwischendaten/filter_doppelte_punkte/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
+lidR::filter_duplicates(ctg)
+ctg2 <- lidR::readLAScatalog("./Daten/Zwischendaten/filter_doppelte_punkte")
+lidR::las_check(ctg2)
+
+#Index für schnelleres Berechnen
+lidR:::catalog_laxindex(ctg2)
+lidR::las_check(ctg2)
+
+
