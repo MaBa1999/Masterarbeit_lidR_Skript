a3331ed9933f473fca047747552ac133dbb4c512
Marc B. <marc.baudy99@gmail.com>
2024-11-20 20:22:30 +0100

    Update Skript_vollständig.R
    Noise

--- Skript_vollständig.R
+++ Skript_vollständig.R
@@ -146,7 +146,7 @@ ggplot2::ggplot(payload(las_tr), aes(X,Z, color = Classification)) +
   scale_color_gradientn(colours = height.colors(50))
 lidR::plot(las)
 
-#Schlife Filterung Noise
+#Schleife Filterung & Löschen: Noise mit sor() & ivf() <- nicht Multicore
 for (i in 1:base::length(ctg2$filename)) {
   las_denoise <- lidR::readLAS(ctg2@data$filename[i])
   las_denoise <- lidR::classify_noise(las_denoise, algorithm = sor())
@@ -162,60 +162,63 @@ for (i in 1:base::length(ctg2$filename)) {
   base::print(i)
 }
 
-#Test
-ctg_test <- lidR::readLAScatalog(c(ctg2@data$filename[1], ctg2@data$filename[2], ctg2@data$filename[3], ctg2@data$filename[4], ctg2@data$filename[5]))
-lidR::opt_laz_compression(ctg_test) <- TRUE
-lidR::las_check(ctg_test)
-lidR::opt_output_files(ctg_test) <- "./Daten/Classify/Noise/sor/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
-lidR::opt_chunk_buffer(ctg_test) <- 30
-lidR::classify_noise(ctg_test, algorithm = sor())
-ctg_test <- lidR::readLAScatalog("./Daten/Classify/Noise/sor/")
-lidR::opt_laz_compression(ctg_test) <- TRUE
-lidR::opt_output_files(ctg_test) <- "./Daten/Classify/Noise/sor/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
-lidR::las_check(ctg_test)
-lidR::opt_chunk_buffer(ctg_test) <- 0
-for (i in 1:length(ctg_test@data$filename)) {
-  test <- lidR::readLAS(ctg_test@data$filename[i])
-  test <-  lidR::filter_poi(test, Classification != LASNOISE)
-  filename <- base::basename(ctg_test$filename[i])
+## Noise - Filterung <- teilweise multicore
+#Filterung mit sor()
+ctg_noise <- ctg2
+lidR::opt_laz_compression(ctg_noise) <- TRUE
+lidR::las_check(ctg_noise)
+lidR::opt_output_files(ctg_noise) <- "./Daten/Classify/Noise/sor/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
+lidR::opt_chunk_buffer(ctg_noise) <- 30
+lidR::classify_noise(ctg_noise, algorithm = sor())
+ctg_noise <- lidR::readLAScatalog("./Daten/Classify/Noise/sor/")
+lidR::opt_laz_compression(ctg_noise) <- TRUE
+lidR::opt_output_files(ctg_noise) <- "./Daten/Classify/Noise/sor/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
+lidR::las_check(ctg_noise)
+lidR::opt_chunk_buffer(ctg_noise) <- 0
+#hier werden die Daten Überschrieben
+for (i in 1:length(ctg_noise@data$filename)) {
+  Noise <- lidR::readLAS(ctg_noise@data$filename[i])
+  Noise <-  lidR::filter_poi(Noise, Classification != LASNOISE)
+  filename <- base::basename(ctg_noise$filename[i])
   filename <- tools::file_path_sans_ext(filename)
   neu_filename <- base::paste0("./Daten/Classify/Noise/sor/", filename, ".laz")
-  lidR::writeLAS(test, neu_filename, index=TRUE)
+  lidR::writeLAS(Noise, neu_filename, index=TRUE)
   base::print(i)
 }
-ctg_test <- lidR::readLAScatalog("./Daten/Classify/Noise/sor/")
-lidR::opt_laz_compression(ctg_test) <- TRUE
-lidR::las_check(ctg_test)
+ctg_noise <- lidR::readLAScatalog("./Daten/Classify/Noise/sor/")
+lidR::opt_laz_compression(ctg_noise) <- TRUE
+lidR::las_check(ctg_noise)
 
-ctg_test <- lidR::readLAScatalog("./Daten/Classify/Noise/sor/")
-lidR::opt_laz_compression(ctg_test) <- TRUE
-lidR::opt_output_files(ctg_test) <- "./Daten/Classify/Noise/ivf/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
-lidR::opt_chunk_buffer(ctg_test) <- 30
-lidR::classify_noise(ctg_test, algorithm = ivf())
-ctg_test <- lidR::readLAScatalog("./Daten/Classify/Noise/ivf/")
-lidR::opt_laz_compression(ctg_test) <- TRUE
-lidR::las_check(ctg_test)
-lidR::opt_chunk_buffer(ctg_test) <- 0
-for (i in 1:length(ctg_test@data$filename)) {
-  test <- lidR::readLAS(ctg_test@data$filename[i])
-  test <-  lidR::filter_poi(test, Classification != LASNOISE)
-  filename <- base::basename(ctg_test$filename[i])
+#Filterung mit ivf()
+ctg_noise <- lidR::readLAScatalog("./Daten/Classify/Noise/sor/")
+lidR::opt_laz_compression(ctg_noise) <- TRUE
+lidR::opt_output_files(ctg_noise) <- "./Daten/Classify/Noise/ivf/Chunks_coordinate_{ID}_{XLEFT}_{YBOTTOM}"
+lidR::opt_chunk_buffer(ctg_noise) <- 30
+lidR::classify_noise(ctg_noise, algorithm = ivf())
+ctg_noise <- lidR::readLAScatalog("./Daten/Classify/Noise/ivf/")
+lidR::opt_laz_compression(ctg_noise) <- TRUE
+lidR::las_check(ctg_noise)
+lidR::opt_chunk_buffer(ctg_noise) <- 0
+#hier werden die Daten Überschrieben
+for (i in 1:length(ctg_noise@data$filename)) {
+  Noise <- lidR::readLAS(ctg_noise@data$filename[i])
+  Noise <-  lidR::filter_poi(Noise, Classification != LASNOISE)
+  filename <- base::basename(ctg_noise$filename[i])
   filename <- tools::file_path_sans_ext(filename)
   neu_filename <- base::paste0("./Daten/Classify/Noise/ivf/", filename, ".laz")
-  lidR::writeLAS(test, neu_filename, index=TRUE)
+  lidR::writeLAS(Noise, neu_filename, index=TRUE)
   base::print(i)
 }
-
-ctg_test <- lidR::readLAScatalog("./Daten/Classify/Noise/ivf/")
-lidR::opt_laz_compression(ctg_test) <- TRUE
-lidR::las_check(ctg_test)
-
+#Überprüfen der Daten 
+ctg_noise <- lidR::readLAScatalog("./Daten/Classify/Noise/ivf/")
+lidR::opt_laz_compression(ctg_noise) <- TRUE
+lidR::las_check(ctg_noise)
 
 ## LasCatalog ohne Noise erstellen
-ctg3 <- lidR::readLAScatalog("./Daten/Classify/Noise")
+ctg3 <- ctg_noise
 lidR::las_check(ctg3)
-lidR:::catalog_laxindex(ctg3)
-lidR::las_check(ctg3)
+#lidR:::catalog_laxindex(ctg3)
+#lidR::las_check(ctg3)
 
 #__________________________________________________________________________________________________________________________________________#
 
